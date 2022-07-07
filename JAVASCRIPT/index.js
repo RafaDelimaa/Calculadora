e2498324c7c263d370bf73b024f851cc477378c9
Rafael De Lima <rafael-silva420@hotmail.com>
2022-07-07 14:08:58 -0300

    calculadora completa

---
+++ JAVASCRIPT/index.js
@@ -0,0 +1,18 @@
+function insert (Number){
+    var numero = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = numero + Number
+}
+
+function clean(){
+    document.getElementById('resultado').innerHTML = "";
+}
+function back(){
+    var resultado = document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1)
+}
+function calcular(){
+    var resultado = document.getElementById('resultado').innerHTML;
+    if(resultado){
+        document.getElementById('resultado').innerHTML = eval(resultado)
+    }
+}
